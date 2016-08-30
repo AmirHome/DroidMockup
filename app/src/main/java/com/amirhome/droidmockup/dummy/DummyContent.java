b7865ccc3058b30f6fb83f805788cd0fc8d6d4a6
Amir Hosseinzadeh <amir.email@yahoo.com>
2016-08-30 12:40:53 +0300

    random status

--- app/src/main/java/com/amirhome/droidmockup/dummy/DummyContent.java
+++ app/src/main/java/com/amirhome/droidmockup/dummy/DummyContent.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Helper class for providing sample content for user interfaces created by
@@ -34,11 +35,33 @@ public class DummyContent {
 
     private static void addItem(DummyItem item) {
         ITEMS.add(item);
+
         ITEM_MAP.put(item.id, item);
     }
 
     private static DummyItem createDummyItem(int position) {
-        return new DummyItem(String.valueOf(position), "Order " + position, makeDetails(position), "Status");
+        Random r = new Random();
+//        int status = r.nextInt(3);
+        String status;
+        switch (r.nextInt(5))
+        {
+            case 0:
+                status = "Accept15";
+                break;
+            case 1:
+                status = "Accept30";
+                break;
+            case 2:
+                status = "Accept45";
+                break;
+            case 3:
+                status = "Accept60";
+                break;
+            default:
+                status = "Reject";
+                break;
+        }
+        return new DummyItem(String.valueOf(position), "Order " + String.valueOf(Integer.valueOf(position) + 1000), makeDetails(position), status);
     }
 
     private static String makeDetails(int position) {
